14ffa5269608f2ddd474bd13466947c8de515319
Irene-Jeongmin-Lee <jmlee21@uw.edu>
2022-02-18 12:07:38 -0800

    Update aggregate.R

--- docs/aggregate.R
+++ docs/aggregate.R
@@ -4,6 +4,14 @@ grouped <- group_by(export, City)
 
 death_per_city <- export %>%
   group_by(City) %>%
-  summarize(
-    num_of_death = nrow(City)
+  summarise(
+    num_of_death = NROW(City)
+  )
+
+grouped <- group_by(export, `Cause of Death`)
+
+types_of_death <- export %>%
+  group_by(`Cause of Death`) %>%
+  summarise(
+    num_per_cause = NROW(`Cause of Death`)
   )
