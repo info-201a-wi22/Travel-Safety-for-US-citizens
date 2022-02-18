4a7439cf4597f73ce6b0a8081168d4aff91f7b2b
jacobdsk <97136844+jacobdsk@users.noreply.github.com>
2022-02-18 00:07:01 -0800

    Update and rename summary-information-script to summary-information-script.R

---
+++ docs/summary-information-script.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+dataset <- read.csv("https://raw.githubusercontent.com/info-201a-wi22/Travel-Safety-for-US-citizens/main/data/export.csv")
+summaryinfo <- list()
+summaryinfo$num_of_causes <- length(unique(dataset$Cause.of.Death))
+
+summaryinfo$most_recent_death_date <- dataset %>% 
+  filter(Date == max(Date, na.rm = TRUE)) %>%
+  filter(Cause.of.Death == "Drowning") %>%
+  pull(Date)
+
+death_count_by_cause <- table(dataset$Cause.of.Death)
+most_death_count <- max(death_count_by_cause)
+summaryinfo$most_common_death_cause  <- names(death_count_by_cause)[which(death_count_by_cause == most_death_count)]
+
+summaryinfo$num_of_locations <- length(unique(dataset$City))
+
+death_count_by_date <- table(dataset$Date)
+summaryinfo$daily_death_avg <- mean(death_count_by_date)
+
+
